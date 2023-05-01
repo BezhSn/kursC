7d44ca5d19ab0f1924469828a4020d2b748e7e29
Snezhana <bezhenar.snezhana@gmail.com>
2023-05-01 12:27:03 +0300

    first commit

---
+++ src/bottom.js
@@ -0,0 +1,11 @@
+document.getElementById("bottom").innerHTML = '\
+<img src="icons/logo.png" alt="logo" class="header_logo bottom_logo">\
+БИБЛИОТЕКА:\
+<a href="https://learn.microsoft.com/ru-ru/dotnet/csharp/" class="bottom_link">Документация по С#</a>\
+<a href="https://ulearn.me/" class="bottom_link">Оcновы С#</a>\
+<a href="https://metanit.com/sharp/tutorial/" class="bottom_link">Полное руководство по С#</a>\
+<a href="https://c-sharp.pro/%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%BD%D0%B8%D0%BA/" class="bottom_link">Задачи по С#</a>\
+YouTube\
+<a href="https://www.youtube.com/watch?v=_8yZYhAkQjQ&list=PLDyJYA6aTY1laYPs6iS-SrYl9DZLVCUKr" class="bottom_link">C# для начинающих</a>\
+<a href="https://www.youtube.com/watch?v=rpyZw0dfpNU" class="bottom_link">WPF и C#</a>\
+'
---
+++ src/header.js
@@ -0,0 +1,4 @@
+document.getElementById("sidebar").innerHTML = '\
+    <img src="icons/logo.png" alt="logo">\
+    <a href="lab_rab.html" class="header_text" >Лабораторные работы по С#</a>\
+    <a href="https://www.onlinegdb.com/online_csharp_compiler" class="header_text" target="_blank">Онлайн - компилятор С#</a>'
